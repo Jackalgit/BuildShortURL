b94acdc0022485251d2382ff38065af7a9f7efa0
guryanov.ivan2 <guryanov.ivan2@wildberries.work>
2023-12-10 15:40:06 +0300

    add config

---
+++ cmd/config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import "flag"
+
+var Config struct {
+	Port        string
+	BaseAddress string
+}
+
+func ConfigPort() {
+	flag.StringVar(&Config.Port, "a", ":8080", "Addres local port")
+
+}
+
+func ConfigBaseAddress() {
+	flag.StringVar(&Config.BaseAddress, "b", "http://localhost:8080/", "Base local addres")
+
+}
